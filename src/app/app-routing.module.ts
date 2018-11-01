28e1149c918ab9f171d2d0f8771f9b008def02f9
AlexKornShell <Ksh_sa@mail.ru>
2018-11-01 11:05:56 +0300

    Dependencies fix. Pages created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+/*  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
+  { path: 'favourites', loadChildren: './favourites/favourites.module#FavouritesPageModule' },
+  { path: 'search', loadChildren: './search/search.module#SearchPageModule' },
+*/
 ];
 
 @NgModule({
